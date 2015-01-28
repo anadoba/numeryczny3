d315fdd0a3754f300c08479a3fbfa9e7c023a0fd
Adam Nadoba <anadoba@windowslive.com>
2015-01-28 09:35:54 +0100

    co 10

--- ZagadnienieRozniczkowe/Program.cs
+++ ZagadnienieRozniczkowe/Program.cs
@@ -26,7 +26,7 @@ namespace ZagadnienieRozniczkowe
             Console.WriteLine("\n\n-----------------------------\n-----\t METODA EULERA \t-----\n-----------------------------");
             Console.WriteLine(ObliczWynik() + "\n");
 
-            Console.WriteLine("\n\n-----------------------------\n---\t ZMOD. METODA EULERA \t---\n-----------------------------");
+            Console.WriteLine("\n\n-----------------------------\n-\t ZMOD. METODA EULERA \t-\n-----------------------------");
             Console.WriteLine(ObliczWynikZmodyfikowanegoEulera() + "\n");
             Console.ReadLine();
         }
@@ -47,26 +47,42 @@ namespace ZagadnienieRozniczkowe
         static void InicjalizacjaZmiennych()
         {
             _h = (_a - 1) / _n;
-            _x = new double[100]; // TODO: ograniczyc wymiar
-            _y = new double[100];
+            _x = new double[1000]; // TODO: ograniczyc wymiar
+            _y = new double[1000];
             _y[0] = 2;
         }
 
+        private static int _coKtory = 0;
         // y[k+1] = y[k] + h*f(x[k],y[k])
-        static void MetodaEulera(int iteracja)
+        static void MetodaEulera(int iteracja, int coKtory)
         {
+            _coKtory++;
             _x[iteracja] = 1 + iteracja*_h;
             _y[iteracja + 1] = _y[iteracja] + _h*F(_x[iteracja], _y[iteracja]);
 
-            Console.WriteLine("x[{0}] = {1}\ty[{0}] = {2}", iteracja, _x[iteracja], _y[iteracja]);
+            if (_coKtory == coKtory)
+            {
+                _coKtory = 0;
+            }
+            if (_coKtory == 1)
+            {
+                Console.WriteLine("x[{0}] = {1}\ty[{0}] = {2}", iteracja, _x[iteracja], _y[iteracja]);
+            }
         }
 
-        static void ZmodyfikowanaMetodaEulera(int iteracja)
+        static void ZmodyfikowanaMetodaEulera(int iteracja, int coKtory)
         {
+            _coKtory++;
             _x[iteracja] = 1 + iteracja * _h;
             _y[iteracja + 1] = _y[iteracja] + _h / 2 * (F(_x[iteracja], _y[iteracja]) + F(_x[iteracja] + _h, _y[iteracja] + _h * F(_x[iteracja], _y[iteracja])));
-
-            Console.WriteLine("x[{0}] = {1}\ty[{0}] = {2}", iteracja, _x[iteracja], _y[iteracja]);
+            if (_coKtory == coKtory)
+            {
+                _coKtory = 0;
+            }
+            if (_coKtory == 1)
+            {
+                Console.WriteLine("x[{0}] = {1}\ty[{0}] = {2}", iteracja, _x[iteracja], _y[iteracja]);
+            }
         }
 
         static double F(double a, double b)
@@ -84,9 +100,9 @@ namespace ZagadnienieRozniczkowe
         {
             int k;
 
-            for (k= 0; k <= _a; k++)
+            for (k= 0; k <= _n; k++)
             {
-                MetodaEulera(k);
+                MetodaEulera(k, _n/10);
             }
 
             k--;
@@ -98,11 +114,12 @@ namespace ZagadnienieRozniczkowe
 
         static double ObliczWynikZmodyfikowanegoEulera()
         {
+            _coKtory = 0;
             int k;
 
-            for (k = 0; k <= _a; k++)
+            for (k = 0; k <= _n; k++)
             {
-                ZmodyfikowanaMetodaEulera(k);
+                ZmodyfikowanaMetodaEulera(k, _n/10);
             }
 
             k--;
